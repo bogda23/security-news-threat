8f727dadd6997c293eae486e13fe89cf59a1ee22
Ivascu Bogdan <ivascubogdan97@gmail.com>
2022-09-01 21:00:13 +0300

    Add typescript ot project

---
+++ src/themes/whiteTheme.js
@@ -0,0 +1,13 @@
+import colorPallete from "./colorPallete";
+
+export default WhiteTheme = {
+
+    colors:{
+        txtColor: colorPallete.darkGrey,
+        titleColor: colorPallete.grey,
+        errorTxtColor: colorPallete.red,
+        bg: colorPallete.white
+
+    }
+  
+}
